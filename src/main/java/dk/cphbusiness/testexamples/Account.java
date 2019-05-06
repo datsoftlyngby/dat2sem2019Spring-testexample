3cac1524eedf8b4bf2a3fb185ca282dd17ac53ef
Anders Kalhauge <anders@kalhauge.dk>
2019-05-06 10:18:53 +0200

    At T1

--- src/main/java/dk/cphbusiness/testexamples/Account.java
+++ src/main/java/dk/cphbusiness/testexamples/Account.java
@@ -1,21 +1,26 @@
 package dk.cphbusiness.testexamples;
 
 public class Account {
+  private final String number;
+  private long balance = 0L;
   
   public Account(String number) {
-    throw new UnsupportedOperationException();
+    this.number = number;
     }
   
   public long getBalance() {
-    throw new UnsupportedOperationException();
+    return balance;
     }
   
   public String getNumber() {
-    throw new UnsupportedOperationException();
+    return number;
     }
 
   public void move(long amount, Account target) throws AccountException {
-    throw new UnsupportedOperationException();
+    if (target == null) throw new AccountException();
+    if (amount < 0L) throw new AccountException();
+    this.balance -= amount;
+    target.balance += amount;
     }
   
   }
--- src/test/java/dk/cphbusiness/testexamples/AccountTest.java
+++ src/test/java/dk/cphbusiness/testexamples/AccountTest.java
@@ -1,5 +1,6 @@
 package dk.cphbusiness.testexamples;
 
+import java.rmi.AccessException;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.is;
@@ -25,6 +26,44 @@ public class AccountTest {
     assertThat(balance, is(0L));
     }
 
+  @Test
+  public void testMoveAmount() throws Exception {
+    Account source = new Account("A38");
+    Account target = new Account("4711");
+    source.move(10000L, target);
+    assertThat(source.getBalance(), is(-10000L));
+    assertThat(target.getBalance(), is(10000L));
+    }
 
+  @Test(expected = AccountException.class)
+  public void testMoveAmountFailsOnNullTarget() throws Exception {
+    Account source = new Account("A38");
+    source.move(10000L, null);
+    }
+  
+  @Test
+  public void testMoveToNullTargetLeavesSourceUntouched() throws Exception {
+    Account source = new Account("A38");
+    try {
+      source.move(10000L, null);
+      }
+    catch (AccountException ae) {
+      assertThat(source.getBalance(), is(0L));
+      }
+    }
+  
+  @Test
+  public void testMoveAmountFailsOnNegativeAmount() throws Exception {
+    Account source = new Account("A38");
+    Account target = new Account("4711");
+    try {
+      source.move(-10000L, target);
+      fail("Should not allow negative amount");
+      }
+    catch (AccountException ae) {
+      assertThat(source.getBalance(), is(0L));
+      assertThat(target.getBalance(), is(0L));
+      }
+    }
   
   }
